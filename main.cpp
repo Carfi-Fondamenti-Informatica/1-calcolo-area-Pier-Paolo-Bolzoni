d8bc3b0d3d6606b07b2d43094d2227649f972097
Pier-Paolo-Bolzoni <100352364+Pier-Paolo-Bolzoni@users.noreply.github.com>
2022-03-14 15:39:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int main(){
     float a=0, b=0, c=0;
     float Triangolo=0, Quadrato=0, Rettangolo=0, Trapezio=0;
+    
     cout<<"Inserisci i tre vertici"<<endl;
     cin >> a >> b >> c;
+    
     Triangolo=(a*b)/2;
     Quadrato=a*a;
     Rettangolo=a*b;
     Trapezio=((a+b)*c)/2;
-    cout<<"l'area del triangolo e' "<<Triangolo<<endl;
-    cout<<"l'area del Quadrato e' "<<Quadrato<<endl;
-    cout<<"l'area del Rettangolo e' "<<Rettangolo<<endl;
-    cout<<"l'area del Trapezio e' "<<Trapezio<<endl;
+    
+    cout << Triangolo << endl;
+    cout << Quadrato << endl;
+    cout << Rettangolo << endl;
+    cout << Trapezio << endl;
+    
     return 0;
 }
